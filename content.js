52a203686446c7570f2b57d9df7677014054253c
Frank Doyle <frankd@Franks-MacBook-Pro-2.local>
2022-08-07 23:53:25 -0500

    show cards

--- content.js
+++ content.js
@@ -25,27 +25,37 @@ observer.observe(document, {
 function updateChildren(node) {
     var paragraphs = node.getElementsByTagName('p')
             for(let p of paragraphs) {
-                // console.log(p.innerHTML)
-                // let capture = "[[(?P<cardname>).*]]"
                 let capture = /\[\[(?<name>.*?)\]\]/g
 
-                
-                // p.innerHTML = p.innerHTML.replaceAll(capture, "FOO BAR BAZ 1 ")
-                
-                
-                p.innerHTML = p.innerHTML.replaceAll(capture, function(match, cardName, offset) {
-                    console.log(match)
-                    console.log(cardName)
+                let matches = p.innerHTML.matchAll(capture)
 
-                    // https://c1.scryfall.com/file/scryfall-cards/normal/front/f/1/f1933d08-07fe-45ea-9b60-d9afb98d5753.jpg?1562855620
-                    
-                    let img = 'https://c1.scryfall.com/file/scryfall-cards/normal/front/f/1/f1933d08-07fe-45ea-9b60-d9afb98d5753.jpg?1562855620'
-                    return `<span style="background:blue;">${cardName}</span><img src=${img}></img>`
-                    // return "||" + cardName + "||"
-                })
+                for(let m of matches) {
+                    let cardName = m.groups.name
+                    if(cardName.includes("cardname")){
+                        break
+                    }
+                    let encodedCardName = encodeURIComponent(cardName)
+                    let url = `https://api.scryfall.com/cards/named?fuzzy=${encodedCardName}`
+                    fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        let toReplace = `[[${cardName}]]`
+                        console.log(data)
+                        let imageUrl = data.image_uris.normal
+                        p.innerHTML = p.innerHTML.replaceAll(toReplace, `
+                        <h3>${cardName}</h3>
+                        <img class="cardimage" src=${imageUrl}></img>
+                        <br/>
+                        `)
+                    });
+                }
 
+                // p.innerHTML = p.innerHTML.replaceAll(capture, function(match, cardName, offset) {
+                //     console.log(match)
+                //     console.log(cardName)
 
-                // p.innerHTML = p.innerHTML.replaceAll("the", "FOO BAR BAZ")
-                // p.innerHTML = "foo"
+                //     let img = 'https://c1.scryfall.com/file/scryfall-cards/normal/front/f/1/f1933d08-07fe-45ea-9b60-d9afb98d5753.jpg?1562855620'
+                //     return `<h4>${cardName}</h4><img src=${img}></img><br/>`
+                // })
             }
 }
